70fc4e29a41ad0afe9c3b59b9b1a56e77da50ff4
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-06-03 16:03:15 -0300

    feat: initial Spring boot project setup

---
+++ sportcenter-api/src/main/java/com/tpfinal/sportcenter_api/SportcenterApiApplication.java
@@ -0,0 +1,13 @@
+package com.tpfinal.sportcenter_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportcenterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportcenterApiApplication.class, args);
+	}
+
+}
---
+++ sportcenter-api/src/test/java/com/tpfinal/sportcenter_api/SportcenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpfinal.sportcenter_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportcenterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
